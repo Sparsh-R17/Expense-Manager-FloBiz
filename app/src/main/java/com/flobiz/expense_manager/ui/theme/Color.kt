df195c2d70e3184deccef7205d99ee0ccb886e4a
Sparsh-R17 <sparsh170702@gmail.com>
2024-12-19 11:37:02 +0530

    Initial : Project Setup
    Added Assets, Firebase Dependencies

---
+++ app/src/main/java/com/flobiz/expense_manager/ui/theme/Color.kt
@@ -0,0 +1,24 @@
+package com.flobiz.expense_manager.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+
+
+val ColorTertiary = Color(0xFF7D5260)
+
+val ColorPrimary = Color(0xFF4C3CCE)
+val ColorPrimaryVariant = Color(0xFFF7F7F7)
+val ColorOnPrimary = Color(0xFFFFFFFF)
+val ColorSecondary = Color(0xFF6F62D8)
+val ColorSecondaryVariant = Color(0xFFF7F7F7)
+val ColorOnSecondary = Color(0xFFFFFFFF)
+val ColorError = Color(0xFFED513F)
+val ColorOnError = Color(0xFFFFFFFF)
+val ColorBackground = Color(0xFFFFFFFF)
+val ColorOnBackground = Color(0xFF353452)
+val ColorSurface = Color(0xFFFFFFFF)
+val ColorOnSurface = Color(0xFF353452)
+val TextColorPrimary = Color(0xFF353452)
+val TextColorSecondary = Color(0xFF767587)
+val TextColorTertiary = Color(0xFFA1A1AB)
+val ColorControlNormal = Color(0xFFA1A1AB)
